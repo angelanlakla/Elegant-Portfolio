22f9dba1d3222da5d2c42a6b279dd510a1a65670
angelanlakla <1340566585@qq.com>
2026-01-31 09:20:00 +0800

    Create vite.config.ts

---
+++ client/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      // 因为文件已经在 client 里面了，所以直接指向 src
+      "@": path.resolve(import.meta.dirname, "src"), 
+      "@shared": path.resolve(import.meta.dirname, "../shared"),
+      "@assets": path.resolve(import.meta.dirname, "../attached_assets"),
+    },
+  },
+  root: path.resolve(import.meta.dirname), // 当前目录就是 root
+  build: {
+    // 这里的输出目录要改为腾讯云默认的 dist
+    outDir: path.resolve(import.meta.dirname, "../dist"), 
+    emptyOutDir: true,
+  }
+});
